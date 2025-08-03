196747e91abe4c1b51499ebc836f00a4fa018a64
EvgeniaDark <vgndrk@gmail.com>
2025-08-04 00:54:03 +0200

    add publick/header

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,3 @@
 import { defineConfig } from "vite";
-import htmlConfig from "vite-plugin-html-config";
 
-export default defineConfig({
-  plugins: [
-    htmlConfig({
-      meta: [
-        {
-          name: "Content-Security-Policy",
-          content:
-            "default-src 'self'; connect-src 'self' https://timer-app-server.onrender.com; script-src 'self'; style-src 'self' 'unsafe-inline';",
-        },
-      ],
-    }),
-  ],
-});
+export default defineConfig({});
